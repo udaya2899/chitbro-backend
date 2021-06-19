e2acf68871252722693cdd9a0253cd99418f41a1
Ferran Sulaiman <ferran.9908@gmail.com>
2021-06-19 19:50:43 +0530

    Push

--- server/contracts/ChitManager.sol
+++ server/contracts/ChitManager.sol
@@ -4,7 +4,7 @@ contract ChitManager {
     uint256 chitLength = 0;
 
     struct Participant {
-	    string walletId;
+        string walletId;
     }
 
     struct Chit {
@@ -18,13 +18,13 @@ contract ChitManager {
         uint256 poolAmount;
         uint256 chitDuration; // Entire chit duration eg. 5 months
         bool roomFilled;
-	    string[] participants;
+        string[] participants;
     }
-
 
     mapping(uint256 => Chit) public chits;
 
     string[] tempParticipantList;
+    string[] usersList;
 
     function createChit(
         string memory _walletId,
@@ -36,7 +36,6 @@ contract ChitManager {
         uint256 poolAmount,
         uint256 chitDuration
     ) public {
-        chitLength++;
         tempParticipantList = [_walletId];
 
         chits[chitLength] = Chit(
@@ -52,13 +51,31 @@ contract ChitManager {
             false,
             tempParticipantList
         );
+        chitLength++;
     }
 
- function getAll() public view returns (Chit[] memory){
-    Chit[] memory res = new Chit[](chitLength+1);
-    for (uint i = 1; i < chitLength+1; i++) {
-        res[i] = chits[i];
+    function getAll() public view returns (Chit[] memory) {
+        Chit[] memory res = new Chit[](chitLength);
+        for (uint256 i = 0; i < chitLength; i++) {
+            res[i] = chits[i];
+        }
+        return res;
     }
-    return res;
-  }
+
+    function joinChit(string memory _walletId, uint256 chitId) public {
+        if (chits[chitId].roomFilled == true) return;
+        uint256 participantListSize = chits[chitId].participants.length;
+        string[] memory newParticipantArray =
+            new string[](participantListSize + 1);
+        for (uint256 i = 0; i < participantListSize; i++) {
+            newParticipantArray[i] = chits[chitId].participants[i];
+        }
+        newParticipantArray[participantListSize] = _walletId;
+        if (newParticipantArray.length >= chits[chitId].roomCapacity) {
+            chits[chitId].roomFilled = true;
+            chits[chitId].participants = newParticipantArray;
+        }
+      
+    }
 }
+
